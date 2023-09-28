7bc5e1d71c63083ad6f41e56db5d7a4fd1aca2dc
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-09-28 05:53:00 -0700

    Updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,12 @@
  *@s: input string
  *Return: always 0
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
 		_putchar('\n');
 	}
-		_puts_recursion(s + 1);
+	_puts_recursion(s + 1);
+	_putchar(*s);
 }
