ec4fe61fab10f16ad6c10acef7c82160274985a1
root <root@DESKTOP-JFSIVR2.localdomain>
2023-10-05 00:15:36 +0300

    Update!

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
+ * str_concat - concatenates two strings & get ends of input and add together
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: NULL on failure otherwise concat of s1 and s2
@@ -18,10 +18,10 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
+
 		for (c = 0; s2[c] != '\0'; c++)
-		con = malloc(sizeof(char) * (i + c + 1));
-	}
+	con = malloc(sizeof(char) * (i + c + 1));
+
 
 	if (con == NULL)
 		return (NULL);
