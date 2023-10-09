65cc06b7cfcd3494d940d25868f5c1c98bcbae10
root <root@DESKTOP-JFSIVR2.localdomain>
2023-10-09 20:00:15 +0300

    New

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@ void jack_bauer(void)
 {
 	int i, m;
 
-	for(i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for(m = 0; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
