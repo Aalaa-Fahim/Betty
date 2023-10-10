f8266d102d138ba14e415d907b0cc79159397ef8
root <root@DESKTOP-JFSIVR2.localdomain>
2023-10-10 22:18:01 +0300

    New

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,12 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- *init_dog - initialize a variable of type struct dog
- *@d: a pointer to the structure
- *@name: the 1st member
- *@age: the 2nd member
- *@owner: the 3rd member
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
