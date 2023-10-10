587d77e22e4244ba86cea4a06ceb94ee17d20da0
root <root@DESKTOP-JFSIVR2.localdomain>
2023-10-10 12:23:46 +0300

    Updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - prints the name of the file it was compiled from
+ * main - prints the name of the file
  *
- *Return: 0 if Success
+ * Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
