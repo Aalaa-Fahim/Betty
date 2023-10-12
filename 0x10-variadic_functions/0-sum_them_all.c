7943b36506646c1483825bfbeefb600f6c4f8b61
root <root@DESKTOP-JFSIVR2.localdomain>
2023-10-12 12:53:48 +0300

    New

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: one of the parameters
+ * @...: A variable number of paramters to calculate the sum of
  *
  *Return: 0 if n == 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	va_start(args, n);
 	unsigned int i, sum = 0;
+
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
