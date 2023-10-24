e37ced273442f9671e0a3560a5ec67a9dfd4b045
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-10-24 13:13:26 +0300

    Updated

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in the linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+ size_t no = 0;
+
+ while (h)
+ {
+ no++;
+ h = h->next;
+ }
+
+ return (no);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of listint_t list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: the address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+ listint_t *node;
+
+ node = malloc(sizeof(listint_t));
+ if (!node)
+ return (NULL);
+
+ node->n = n;
+ node->next = *head;
+ *head = node;
+
+ return (node);
+}
