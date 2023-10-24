7e9e192b07ca3aec0b140c4b8d4bd15beddff117
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-10-24 20:59:09 +0300

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t no = 0;
+	size_t no = 0;
 
-while (h)
-{
-	printf("%d\n", h->n);
-	no++;
-	h = h->next;
-}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		no++;
+	}
 
-return (no);
+	return (no);
 }
