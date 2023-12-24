91e971e93c8068ba4f8fc7da68467efedefdd6d1
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-12-24 13:47:29 +0200

    Updated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,18 +9,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-unsigned long int i;
-hash_node_t *tmp;
+	unsigned long int i;
+	hash_node_t *tmp;
 
-if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
 	return (NULL);
-i = key_index((const unsigned char *)key, ht->size);
-tmp = ht->array[i];
-while (tmp != NULL)
-{
-	if (strcmp(tmp->key, key) == 0)
-		return (tmp->value);
-	tmp = tmp->next;
-}
-return (NULL);
 }
