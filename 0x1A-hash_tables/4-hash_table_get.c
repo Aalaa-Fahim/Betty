7d7f7d6b1fadb8652ed5a5b725d5d4c7107c0780
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-12-24 07:37:56 +0200

    Updated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,8 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 unsigned long int i;
 hash_node_t *tmp;
 
-if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-    key == NULL || strlen(key) == 0)
+if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
 	return (NULL);
 i = key_index((const unsigned char *)key, ht->size);
 tmp = ht->array[i];
